f25a529584d8c8c977c4c6caa34db6bbf5b7a636
Yuta-Iwase <yutabk201@gmail.com>
2019-02-25 00:17:29 +0900

    sfn

---
+++ src/ScaleFreeNetwork.java
@@ -0,0 +1,21 @@
+
+public class ScaleFreeNetwork extends Network{
+
+	/**
+	 * scale-free networkを生成する。<br>
+	 * 与えられた条件で、ネットワーク生成を成功するまで繰り返す。<br>
+	 * @param N 頂点数
+	 * @param gamma べき指数
+	 * @param minDegree 最小次数
+	 * @param maxDegree 最大次数
+	 * @param loopLimit 生成時のループ数の許容値
+	 * @param seed 乱数シード
+	 */
+	public ScaleFreeNetwork(int N, double gamma, int minDegree, int maxDegree, int loopLimit, long seed) {
+		do {
+			MakePowerLaw dist = new MakePowerLaw(N, gamma, minDegree, maxDegree);
+			ConfigurationNetwork.generate(this, dist.degree, loopLimit, seed);
+		}while(!success);
+	}
+
+}
